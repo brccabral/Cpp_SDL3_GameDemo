99d834e41399573a1ea7009cd2da602a8e349fd3
Bruno Cabral <cabral.brc@gmail.com>
2025-08-29 17:43:21 -0700

    animation class

---
+++ game/animation.cpp
@@ -0,0 +1,16 @@
+#include "animation.hpp"
+
+float Animation::getLength() const
+{
+    return timer.getLength();
+}
+
+int Animation::currentFrame() const
+{
+    return static_cast<int>(timer.getTime() / timer.getLength() * frameCount);
+}
+
+void Animation::step(const float deltaTime)
+{
+    return timer.step(deltaTime);
+}
---
+++ game/animation.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "timer.hpp"
+
+class Animation
+{
+    Timer timer{0};
+    int frameCount{};
+
+public:
+    Animation() = default;
+
+    Animation(const int frameCount, const float length) : timer(length), frameCount(frameCount)
+    {
+    };
+
+    [[nodiscard]] float getLength() const;
+    [[nodiscard]] int currentFrame() const;
+    void step(float deltaTime);
+};
